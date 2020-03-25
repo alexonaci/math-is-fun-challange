d13b496d13b8c4ea17297d6a494c5dfb658b9195
Adrian Hodrea <adihodrea@yahoo.com>
2020-03-25 15:54:30 +0200

    added calculate and display result functionality + some minor fine-tunning

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const calculateButton = document.querySelector('button');
 
 const checkAllInputElementsOk = () => {
     let allOK = true;
-    Object.values(inputElementsValidState).forEach( (state)=> {
+    Object.values(inputElementsValidState).forEach((state) => {
         if (state === "invalid") {
             allOK = false;
         }
@@ -14,19 +14,15 @@ const checkAllInputElementsOk = () => {
 }
 
 const validateInputIsNumber = (value) => {
-    if (value) {
-        const valNumber = parseInt(value);
-        if (valNumber == value) {
-            return true;
-        } else {
-            return false;
-        }    
+    const valNumber = parseInt(value);
+    if (valNumber == value) {
+        return true;
     } else {
         return false;
     }
 }
 
-const alertIfNotANumber = ({target}) => {
+const alertIfNotANumber = ({ target }) => {
     if (!validateInputIsNumber(target.value)) {
         target.classList.add('inputNotValid');
         inputElementsValidState[target.id] = 'invalid';
@@ -36,23 +32,67 @@ const alertIfNotANumber = ({target}) => {
     }
     if (checkAllInputElementsOk()) {
         calculateButton.classList.remove('hideElement');
-        errMsgView.innerText = 'All clear. No Errors ! :]';   
+        errMsgView.innerText = 'All clear. No Errors ! :]';
         errMsgView.classList.remove('inputNotValid');
 
     } else {
         calculateButton.classList.add('hideElement');
-        errMsgView.innerText = 'All input fields whould have valid numbers (inlcuding 0)';   
+        errMsgView.innerText = 'All input fields should have valid numbers (inlcuding 0)';
         errMsgView.classList.add('inputNotValid');
-    }  
+    }
 }
 
-const inputElements = document.getElementsByTagName('input');
-const inputElementsArr = [...inputElements];
-console.dir(inputElements);
-console.dir(inputElementsArr);
-inputElementsArr.forEach(field => {
+const getOperationSimbol = (operationValue) => {
+    let operation = '';
+    switch (operationValue) {
+        case 'addition':
+            operation = '+';
+            break;
+        case 'substraction':
+            operation = '-';
+            break;
+        case 'division':
+            operation = '/';
+            break;
+        case 'multiplication':
+            operation = '*';
+            break;
+    }
+    return operation;
+}
+
+const calculateResult = () => {
+    let resultString = '';
+    const operandsAndOperations = [...document.querySelectorAll("input, select")];
+    operandsAndOperations.forEach((element) => {
+        if (element.tagName === "INPUT") {
+            resultString += parseInt(element.value);
+        } else {           // operation
+            const operation = getOperationSimbol(element.value);
+            resultString += operation;
+        }
+    })
+    return eval(resultString);
+}
+
+const displayResult = (view, result) => {
+    view.innerText = result;
+}
+
+const inputElements = [...document.getElementsByTagName('input')];
+inputElements.forEach(field => {
+    field.value = '0';
     field.addEventListener("input", () => {
         alertIfNotANumber(event);
-    } )
+    })
 })
+
+const calculateAndDisplayResult = () => {
+    const result = calculateResult();
+    displayResult(resultView, result);
+
+}
+
+calculateButton.addEventListener("click", calculateAndDisplayResult);
+
 
